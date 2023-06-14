169bf19564c827a4f3e0f283516a2e7509063b08
Lehman3D <davilehman@gmail.com>
2023-06-13 19:21:17 -0500

    Initial Set up

---
+++ server.js
@@ -0,0 +1,26 @@
+//check if we are running in the prod or dev environment
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const mongoose = require('mongoose')
+const indexRouter = require('./routes/index')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+//connect to db
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
+
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT  || 3000)
