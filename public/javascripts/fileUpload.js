b033c0e318d00beab7eb20d1b85c759e2ab5d809
Lehman3D <davilehman@gmail.com>
2023-06-27 18:14:43 -0500

    finished the main backend and half front end

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,13 +1,30 @@
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode,
-)
+const rootStyles = window.getComputedStyle(document.documentElement)
 
-FilePond.setOptions({
-    stylePanelAspectRatio: 150 / 100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150
-})
+if (rootStyles.getPropertyValue('--event-image-width-large') != null && rootStyles.getPropertyValue('--event-image-width-large') != '') {
+    ready()
+} else {
+    document.getElementById('main-css').addEventListener('load', ready)
+}
 
-FilePond.parse(document.body);
+function ready() {
+    const imageWidth = parseFloat(rootStyles.getPropertyValue('--event-image-width-large'))
+    const imageAspectRatio = parseFloat(rootStyles.getPropertyValue('--event-image-aspect-ratio'))
+    const imageHeight = imageWidth / imageAspectRatio
+
+
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize,
+        FilePondPluginFileEncode,
+    )
+    
+    FilePond.setOptions({
+        stylePanelAspectRatio: 1 / imageAspectRatio,
+        imageResizeTargetWidth: imageWidth,
+        imageResizeTargetHeight: imageHeight
+    })
+    
+    FilePond.parse(document.body);
+}
+
+
